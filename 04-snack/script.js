9fc85a29608f73457413a28d51b08d1fd75c43c2
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-05-30 12:46:36 +0200

    Implement logic to collect odd numbers from user input and store them in an array

---
+++ 04-snack/script.js
@@ -0,0 +1,12 @@
+// Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
+console.log("ciao");
+
+const numbersUneven = [];
+for (let i = 0; i < 6; i++) {
+    const prompts = Number(prompt("Scrivi un numero"));
+    console.log(prompts);
+    if (prompts % 2 != 0) {
+        numbersUneven.push(prompts);
+    }
+}
+console.log(numbersUneven);
