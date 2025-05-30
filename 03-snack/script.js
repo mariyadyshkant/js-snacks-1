5df18d137fb9f4ad1d6bb3afa5b9683bf0fdc91f
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-05-30 12:48:18 +0200

    Fix sum calculation logic and store user inputs in an array

--- 03-snack/script.js
+++ 03-snack/script.js
@@ -1,14 +1,15 @@
 // Il soft deve chiedere per 10 volte all'utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
 const numbers = [];
 for (let i = 0; i <= 10; i++) {
-    const prompts = Number(prompt("Scrivi un numero"));
-    numbers.push(prompts);
-}
-console.log(numbers);
-
-
-for (let i = 0; i < numbers.length; i++) {
-    somma += numbers[i];
+    let prompts = Number(prompt("Scrivi un numero"));
+    somma += prompts[i];
 }
 console.log(somma);
+
+
+// for (let i = 0; i < numbers.length; i++) {
+//     somma += numbers[i];
+// }
+// console.log(somma);
 
