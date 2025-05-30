2b5a4ab48516575f3f917c4f4dd77d6e56b26fa8
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-05-30 10:52:34 +0200

    Add initial HTML and JavaScript files for number comparison functionality

---
+++ 01-snack/script.js
@@ -0,0 +1,11 @@
+//1. L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+const numberOne = Number(prompt("Scrivi un numero"));
+const numberTwo = Number(prompt("Scrivi un secondo numero"));
+if (numberOne > numberTwo) {
+    console.log(numberOne);
+
+}
+else {
+    console.log(numberTwo);
+
+}
