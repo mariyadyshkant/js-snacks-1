85fe187739b5962b57d2b386f866580c9b0e69a5
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-05-30 12:17:50 +0200

    Refactor number input logic to use a loop for user prompts and simplify array creation

--- 03-snack/script.js
+++ 03-snack/script.js
@@ -1,19 +1,11 @@
 // Il soft deve chiedere per 10 volte all'utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
-
-const nOne = Number(prompt("Scrivi un numero"));
-const nTwo = Number(prompt("Scrivi un numero"));
-const nThree = Number(prompt("Scrivi un numero"));
-const nFour = Number(prompt("Scrivi un numero"));
-const nFive = Number(prompt("Scrivi un numero"));
-const nSix = Number(prompt("Scrivi un numero"));
-const nSeven = Number(prompt("Scrivi un numero"));
-const nEight = Number(prompt("Scrivi un numero"));
-const numberNine = Number(prompt("Scrivi un numero"));
-const nTen = Number(prompt("Scrivi un numero"));
-
-const numbers = [nOne, nTwo, nThree, nFour, nFive, nSix, nSeven, nEight, numberNine, nTen];
+const numbers = [];
+for (let i = 0; i <= 10; i++) {
+    const prompts = Number(prompt("Scrivi un numero"));
+    numbers.push(prompts);
+}
 console.log(numbers);
-let somma = 0;
+
 
 for (let i = 0; i < numbers.length; i++) {
     somma += numbers[i];
