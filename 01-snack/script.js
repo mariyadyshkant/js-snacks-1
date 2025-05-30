095b1c503ba0508108a3840f6b7336b8424e3233
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-05-30 10:58:55 +0200

    Fix comparison logic to prompt user for different numbers

--- 01-snack/script.js
+++ 01-snack/script.js
@@ -1,7 +1,11 @@
 //1. L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
 const numberOne = Number(prompt("Scrivi un numero"));
 const numberTwo = Number(prompt("Scrivi un secondo numero"));
-if (numberOne > numberTwo) {
+if (numberOne == numberTwo) {
+    console.log("Scrivi due numeri diversi, grazie");
+
+}
+else if (numberOne > numberTwo) {
     console.log(numberOne);
 
 }
