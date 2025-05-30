0e9f7f3e906f1add31ddb503b98fddb7240b9243
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-05-30 13:01:48 +0200

    Add task number to comments for clarity in user input prompts

--- 03-snack/script.js
+++ 03-snack/script.js
@@ -1,4 +1,4 @@
-// Il soft deve chiedere per 10 volte all'utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+// 3. Il soft deve chiedere per 10 volte all'utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
 
 const numbers = [];
 for (let i = 0; i <= 10; i++) {
